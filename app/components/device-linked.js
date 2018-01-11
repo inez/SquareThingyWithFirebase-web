6cb67f2359fadf56f5bd1f99fa4ea05cc5ce59fa
Inez Korczynski <inez@squareup.com>
2018-01-10 17:30:24 -0800

    ...

--- app/components/device-linked.js
+++ app/components/device-linked.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+    didInsertElement() {
+        this._super(...arguments);
+
+        var ref = firebase.database().ref('transactions/' + this.get('id'));
+            ref.on('child_added', snapshot => {
+                this.set('isProcessing', false);
+                this.set('showSuccess', true);
+                Ember.run.later(() => {
+                    this.set('showSuccess', false);
+                }, 2500);
+            });
+    },
+
 	id: null,
 
 	amount: null,
@@ -11,15 +24,6 @@ export default Ember.Component.extend({
         	var ref = firebase.database().ref('chargeRequests/' + this.get('id'));
         	ref.child(ref.push().key).set(this.get('amount'));
         	this.set('isProcessing', true);
-
-        	var ref = firebase.database().ref('transactions/' + this.get('id'));
-        	ref.on('child_added', snapshot => {
-        		this.set('isProcessing', false);
-        		this.set('showSuccess', true);
-        		Ember.run.later(() => {
-        			this.set('showSuccess', false);
-        		}, 2500);
-        	});
         }
     }
 
